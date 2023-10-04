81c8b1e6838edb226a199101b96dd2494ecce465
fasaldana <fernando_s_98@hotmail.com>
2023-10-04 14:57:30 -0500

    Fix app.module.ts to match the new service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommitHistoryBackService } from './commit-history-back/commit-history-back.service';
+import { CommitHistoryBackController } from './commit-history-back/commit-history-back.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, CommitHistoryBackController],
+  providers: [AppService, CommitHistoryBackService],
 })
 export class AppModule {}
